c047dd936fa48c16ae71bb8ef56a6f4d974d56a4
etapa-racional <111235343+etapa-racional@users.noreply.github.com>
2023-11-09 18:47:04 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 phpinfo();
+
+$servername = getenv(strtoupper(getenv("DATABASE_SERVICE_NAME"))."_SERVICE_HOST");
+$database =  getenv("DATABASE_NAME");
+$username = getenv("DATABASE_USER");
+$password = getenv("DATABASE_PASSWORD");
+
+$db = pg_pconnect("host=". $servername ."dbname=". $database ."user=".$username ."password=".$password);
+$qr = "SELECT * FROM sampletb;";
+$rs = pg_query($db, $qr);
+while ($rw = pg_fetch_assoc($rs)) {
+        echo "<p>".$rw['xxx']."</p>";
+}
 ?>
