dcff136463272190a298c6fc1d9c1abf4c0e0809
etapa-racional <111235343+etapa-racional@users.noreply.github.com>
2023-11-09 00:36:38 +0000

    Update views.py

--- welcome/views.py
+++ welcome/views.py
@@ -19,6 +19,20 @@ def index(request):
         'count': PageView.objects.count()
     })
 
+    import psycopg2
+    import psycopg2.extras
+    import datetime
+    import time
+    conn = psycopg2.connect(
+        host="postgresql.etapa-racional-dev.svc.cluster.local",
+        database="sampledb",
+        user="userPV3",
+        password="6kyWtNgL76ekv0kF",
+        port="5432")
+    conn.autocommit = True
+    cur = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    cur.execute("INSERT INTO sampletb (xxx) VALUES ('"+datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+"');")
+
 def health(request):
     """Takes an request as a parameter and gives the count of pageview objects as reponse"""
     return HttpResponse(PageView.objects.count())
