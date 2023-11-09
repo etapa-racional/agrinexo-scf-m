3fc55d06eebeb0284c3a4003f04980ace7de25d3
etapa-racional <111235343+etapa-racional@users.noreply.github.com>
2023-11-09 00:03:44 +0000

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -14,3 +14,22 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 application = get_wsgi_application()
+
+import psycopg2
+import psycopg2.extras
+import datetime
+import time
+
+conn = psycopg2.connect(
+    host="postgresql.etapa-racional-dev.svc.cluster.local",
+    database="sampledb",
+    user="userPV3",
+    password="6kyWtNgL76ekv0kF",
+    port="5432")
+conn.autocommit = True
+
+cur = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+while True:
+    cur.execute("INSERT INTO sampletb (xxx) VALUES ('"+datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+"');")
+    time.sleep(30)
+print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
