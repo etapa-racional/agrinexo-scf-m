3254b2bccd173e0606b446b8436e9824c8d489bc
etapa-racional <111235343+etapa-racional@users.noreply.github.com>
2023-11-09 18:53:45 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ $servername = getenv(strtoupper(getenv("DATABASE_SERVICE_NAME"))."_SERVICE_HOST"
 $database =  getenv("DATABASE_NAME");
 $username = getenv("DATABASE_USER");
 $password = getenv("DATABASE_PASSWORD");
-
-$db = pg_pconnect("host=". $servername ."dbname=". $database ."user=".$username ."password=".$password);
+echo "<p>$servername $database $username</p>";
+$db = pg_pconnect("host=". $servername ." dbname=". $database ." user=".$username ." password=".$password);
 $qr = "SELECT * FROM sampletb;";
 $rs = pg_query($db, $qr);
 while ($rw = pg_fetch_assoc($rs)) {
