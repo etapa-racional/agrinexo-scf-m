81625d5555ffc9b29fdfaa4e0774c70c4a768cfa
etapa-racional <111235343+etapa-racional@users.noreply.github.com>
2023-11-09 13:55:05 +0000

    Add files via upload

--- welcome/views.py
+++ welcome/views.py
@@ -18,13 +18,14 @@ def index(request):
     PageView.objects.create(hostname=hostname)
     
     conn = psycopg2.connect(
-        host="postgresql.etapa-racional-dev.svc.cluster.local",
-        database="sampledb",
-        user="userPV3",
-        password="6kyWtNgL76ekv0kF",
+        host=os.getenv('{}_SERVICE_HOST'.format(service_name)),
+        database= os.getenv('DATABASE_NAME'),
+        user=os.getenv('DATABASE_USER'),
+        password=os.getenv('DATABASE_PASSWORD'),
         port="5432")
     conn.autocommit = True
     cur = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    cur.execute("CREATE TABLE IF NOT EXISTS sampletb (xxx VARCHAR(50));")
     cur.execute("INSERT INTO sampletb (xxx) VALUES ('"+datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+"');")  
     
     return render(request, 'welcome/index.html', {
