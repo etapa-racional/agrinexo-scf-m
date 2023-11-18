298dbbd8283ce7eaa5400459e9d5d646ba126319
etapa-racional <etapa-racional@etapa-racional.pt>
2023-11-18 14:50:27 +0000

    Backend bug fixes.

---
+++ datasources/getNCEP.py
@@ -0,0 +1,29 @@
+import cdsapi
+
+c = cdsapi.Client()
+
+c.retrieve(
+    'seasonal-postprocessed-single-levels',
+    {
+        'format': 'grib',
+        'originating_centre': 'ncep',
+        'system': '2',
+        'variable': [
+            '2m_temperature_anomaly', 'maximum_2m_temperature_in_the_last_24_hours_anomaly',
+            'minimum_2m_temperature_in_the_last_24_hours_anomaly',
+            'total_precipitation_anomalous_rate_of_accumulation',
+        ],
+        'product_type': 'ensemble_mean',
+        'year': ['2020', '2021', '2022'],
+        'month': [
+            '01', '02', '03',
+            '04', '05', '06',
+            '07', '08', '09',
+            '10', '11', '12',
+        ],
+        'leadtime_month': [
+            '1', '2', '3',
+            '4', '5', '6',
+        ],
+    },
+    'NCEP.grib')
